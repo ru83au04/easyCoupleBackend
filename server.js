0a1228105d591fa52e8d9844d31fe08887212c65
noah <noah@gmail.com>
2024-12-05 06:46:53 +0800

    加入SSL測試，使用HTTPS連線

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 // server.js
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // 設置靜態文件夾
-const DIST_DIR = path.join(__dirname, 'dist/angular_capacitor_2/browser');
+const DIST_DIR = path.join(__dirname, 'public/angular_capacitor_2/browser');
 app.use(express.static(DIST_DIR));
+
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'ssl/server.key')),
+  cert: fs.readFileSync(path.join(__dirname,'ssl/server.crt'))
+}
 
 // 處理所有路由，返回 Angular 應用的 index.html 文件
 app.get('*', (req, res) => {
   res.sendFile(path.join(DIST_DIR, 'index.html'));
 });
 
-// 監聽特定端口
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// app.get('/', (req, res) => {
+//   res.send('Hello from HTTPS Server!');
+// });
+
+https.createServer(httpsOptions, app).listen(3000, () => {
+  console.log('HTTPS Server running on port 3000');
 });
+
+// // 監聽特定端口
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
