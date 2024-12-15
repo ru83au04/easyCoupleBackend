eea74da7c5ae7257b2e7e57d58935613cbd85679
noah <noah@gmail.com>
2024-12-15 17:43:17 +0800

    調整參數

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const getWeather = async (req, res) => {
     const lat = req.query.lat;
-    const lon = req.query.lng;
+    const lon = req.query.lon;
     console.log("location", lat, lon);
     console.log("key", process.env.WEATHER_KEY);
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_KEY}&units=metric&lang=en`;
