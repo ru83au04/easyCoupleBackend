448990cb012ec2cbb5a4097f48191eb25d206b37
noah <noah@gmail.com>
2024-12-15 17:39:26 +0800

    測試

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -5,12 +5,14 @@ const axios = require('axios');
 const getWeather = async (req, res) => {
     const lat = req.query.lat;
     const lon = req.query.lng;
+    console.log("location", lat, lon);
+    console.log("key", process.env.WEATHER_KEY);
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_KEY}&units=metric&lang=en`;
     try{
         const response = await axios.get(weatherApi);
         res.json(response.data);
     }catch(err){
-        console.log('error', err);
+        console.log('backend error', err);
     }
 }
 
