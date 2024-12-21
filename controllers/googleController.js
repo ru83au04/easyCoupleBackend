e8cf09ff420b4c6b5c129ee8e4c82098728ff263
noah <noah@gmail.com>
2024-12-21 15:26:43 +0800

    後端API調整

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -13,7 +13,7 @@ const findFood = async (req, res) => {
     // 調用 Google Places API
     const response = await axios.post(url,{
         includedPrimaryTypes: ["restaurant", "cafe", "bar"],
-        maxResultCount: 30,            // 返回最大數量
+        // maxResultCount: 30,            // 返回最大數量
         locationRestriction: {         // 限制範圍
             circle: {
             center: {
