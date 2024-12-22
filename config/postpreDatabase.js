0ea5bb3feb0a6be00a050da0589682ed37517592
noah <noah@gmail.com>
2024-12-22 14:57:07 +0800

    修正資料庫搜尋方式與參數

--- config/postpreDatabase.js
+++ config/postpreDatabase.js
@@ -93,10 +93,10 @@ async function insertRecords(records) {
 }
 
 async function getData(param){
-  let paramQuery = `SELECT * FROM trash_collection_points WHERE "AREA" = '${param}'`
-  let result = await pool.query(paramQuery);
-  console.log("result", result);
-  return result;
+  let paramQuery = `SELECT * FROM trash_collection_points WHERE AREA = $1`
+  let result = await pool.query(paramQuery, [param]);
+  console.log("result", result.rows);
+  return result.rows;
 }
 
 module.exports = { pool, initDatabase, getData}
