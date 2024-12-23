22020deb02629946aacebc8fdd97adc9b1fa32b9
noah <noah@gmail.com>
2024-12-23 21:36:05 +0800

    修改CSV匯入資料庫方法

--- config/postpreDatabase.js
+++ config/postpreDatabase.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');  // 使用 PostgreSQL 的客戶端
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();  // 載入 .env 檔案中的環境變數
 const axios = require('axios');
 
@@ -8,8 +9,46 @@ const pool = new Pool({
 });
 
 async function initDatabaseWithCsv() {
-  let pathUrl = '/opt/render/project/src/public/assets/TrashRoutes.csv';
-  copyCsvFile(pathUrl);
+  const insertQuery = `
+  INSERT INTO trash_collection_points (
+    area, route_id, route_order, village, point_name, time, longitude, latitude, word_day, recycle_day
+  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+  ON CONFLICT (area, route_id, route_order) DO NOTHING;
+`;
+
+const client = await pool.connect();
+const fileUrl = path.join(__dirname, '../public/assets/TrashRoutes.csv');
+
+try {
+  const rows = [];
+  fs.createReadStream(fileUrl)
+    .pipe(csv())
+    .on('data', (row) => {
+      rows.push(row);
+    })
+    .on('end', async () => {
+      for (const row of rows) {
+        const values = [
+          row.AREA,
+          row.ROUTEID,
+          parseInt(row.ROUTEORDER, 10),
+          row.VILLAGE,
+          row.POINTNAME,
+          row.TIME,
+          parseFloat(row.LONGITUDE),
+          parseFloat(row.LATITUDE),
+          row.WORDDAY,
+          row.RECYCLEDAY,
+        ];
+        await client.query(insertQuery, values);
+      }
+      console.log('資料匯入成功');
+    });
+  } catch (error) {
+    console.error('匯入資料失敗:', error);
+  } finally {
+    client.release();
+  }
 }
 
 async function initDatabase() {
@@ -103,24 +142,6 @@ async function getData(param){
   let result = await pool.query(paramQuery);
   console.log("result", result.rows);
   return result.rows;
-}
-
-async function copyCsvFile(filePath){
-  const query = `
-    COPY trash_position(AREA,ROUTEID,ROUTEORDER,VILLAGE,POINTNAME,TIME,LONGITUDE,LATITUDE,WORDDAY,RECYCLEDAY)
-    FROM '${filePath}'
-    DELIMITER ','
-    CSV HEADER;
-  `;
-
-  try {
-    const client = await pool.connect();
-    await client.query(query);
-    console.log('CSV 文件成功匯入資料庫！');
-    client.release();
-  } catch (error) {
-    console.error('匯入資料失敗：', error);
-  }
 }
 
 module.exports = { pool, initDatabase, initDatabaseWithCsv, getData}
