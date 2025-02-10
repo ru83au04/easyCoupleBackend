3eb7c50021a827c31bcf54dbe2ff3f84fe474cc1
ru83au04 <tony151911@gmail.com>
2025-02-10 22:39:21 +0800

    更新前端內容

---
+++ public/front_end/browser/chunk-6UQW3UTH.js
@@ -0,0 +1,1 @@
+import{Cb as a,Db as o,Vb as c,jb as n}from"./chunk-IQ45D3DL.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-attendance"]],decls:2,vars:0,template:function(e,s){e&1&&(a(0,"p"),c(1,"attendance works!"),o())},encapsulation:2})};export{p as AttendanceComponent};
