81391080df87651a21133ae0bf917a7132237eb3
ru83au04 <tony151911@gmail.com>
2025-02-13 07:34:00 +0800

    調整前端RWD內容趴兔

---
+++ public/front_end/browser/chunk-MT653GXR.js
@@ -0,0 +1,1 @@
+import{Bb as a,Cb as o,Ub as c,ib as n}from"./chunk-Y36Y4OMK.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-attendance"]],decls:2,vars:0,template:function(e,s){e&1&&(a(0,"p"),c(1,"attendance works!"),o())},encapsulation:2})};export{p as AttendanceComponent};
